93e6affd5516c8576bd01590a3cf80f84b5403fc
listonaj <jm.romain@outlook.com>
2022-06-04 13:23:27 -0700

    binary tree impl

---
+++ my_binarytree.py
@@ -0,0 +1,13 @@
+from BinaryTree import*
+
+newBT = TreeNode("Drinks")
+leftChild = TreeNode("Hot")
+tea = TreeNode("Tea")
+coffee = TreeNode("Coffee")
+leftChild.leftChild = tea
+leftChild.rightChild = coffee
+rightChild = TreeNode("Cold")
+newBT.leftChild = leftChild
+newBT.rightChild = rightChild
+
+inOrderTraversal(newBT)
